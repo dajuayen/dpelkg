3d883995bfe11b39c7b770046a9a49c52a244139
David Juaneda <d.juaneda@avoristravel.com>
2019-05-22 16:59:54 +0200

    [COMPLETED] Writing your first Django app, part 7

--- connectionELK/polls/urls.py
+++ connectionELK/polls/urls.py
@@ -2,13 +2,23 @@ from django.urls import path
 
 from . import views
 
+# Namespace
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/old
+    path('old', views.index, name='indexOld'),
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/old/5/
+    path('old/<int:question_id>/', views.detail, name='detailOld'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/s/5/
+    path('s/<int:question_id>/', views.shortcutDetail, name='shortcut_detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/old/5/results/
+    path('old/<int:question_id>/results/', views.results, name='resultsOld'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
